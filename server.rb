89206fa5e7954a8f819daf7b7fc9c5f379f2eab7
ilyana19 <aries_2152@hotmail.com>
2018-08-01 14:29:51 -0400

    added favourite links

--- server.rb
+++ server.rb
@@ -9,5 +9,20 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'music', 'games', 'anime/manga']
+
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = [
+    'https://old.reddit.com/r/Guildwars2',
+    'https://www.guildwars2.com',
+    'https://www.twitch.tv/mightyteapot',
+    'https://www.dndbeyond.com',
+    'https://roll20.net'
+  ]
+
+  erb :favourites
 end
